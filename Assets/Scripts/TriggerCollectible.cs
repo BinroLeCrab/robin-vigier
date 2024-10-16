4aaf9f219c8144b516f03f877bd1f091bde4a03b
Robin Vigier / B!nro <rob.vigier@gmail.com>
2024-10-16 12:31:36 +0200

    🚧 Trigger collision

---
+++ Assets/Scripts/TriggerCollectible.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+[RequireComponent(typeof(gameObject))]
+
+public class TriggerCollectible : MonoBehaviour
+{
+    [SerializeField] private CharacterController m_Player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
